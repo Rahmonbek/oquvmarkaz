72b7a8c7c9f73c66afb7b4355da64b7db24a9efb
Diyorbek5 <davlatovdiyorbek15@gmail.com>
2021-07-18 16:44:29 +0500

    User_cabinet
    z

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import User from './pages/user_cabinet/User'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import User from './pages/user_cabinet/User'
 
 export default class App extends Component {
   render() {
