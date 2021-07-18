57d5f081e88c4564a3a9940e9fd3f30c579c44d5
Diyorbek5 <davlatovdiyorbek15@gmail.com>
2021-07-18 15:53:55 +0500

    user
    z

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import User from './pages/user_cabinet/User'
+
 export default class App extends Component {
   render() {
     return (
       <div>
-     Hello React
-           </div>
+        <User />
+      </div>
     )
   }
 }
