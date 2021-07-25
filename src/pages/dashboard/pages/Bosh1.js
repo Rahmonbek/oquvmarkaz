1b5ba7a37761ee0fdf4bb1ccf0c8fe860e84a45c
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-07-25 23:21:53 +0500

    menyu ozgardi

--- src/pages/dashboard/pages/Bosh1.js
+++ src/pages/dashboard/pages/Bosh1.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
-import { Form,FormControl,InputGroup } from 'react-bootstrap'
+import { Col, Form,FormControl,InputGroup, Row } from 'react-bootstrap'
 import stayle from '../css/Navbar.module.css';
-import img1 from '../img/dastur.png';
-import img2 from '../img/siz.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import ReactStars from "react-rating-stars-component";
 
 export default class Bosh1 extends Component {
     
     
     render() {
+        const ratingChanged = (newRating) => {
+            console.log(newRating);
+          };  
         return (
             <div>
                 <div id={stayle.all} expand="lg"  className="mr-auto my-2 my-lg-0">
 <div className={stayle.elips}></div>
-<div style={{width:'46%', backgroundColor:'red'}}><div className={stayle.dastur}>
+<div style={{width:'46%',}}><div className={stayle.dastur}>
 <p style={{fontWeight:'800',fontSize:'2.6vw'}}>Biz sizga <span style={{color:'#e9e50a',fontWeight:'800',fontSize:'2.6vw'}}>Dasturla</span>|</p>
 <span style={{fontWeight:'800',fontSize:'2.6vw',width:'100%'}}>va boshqa kurslarni taklif 
 qilamiz.</span>
@@ -22,7 +23,7 @@ qilamiz.</span>
 
 
 
-<div className={stayle.dastur1}>
+<div style={{color:'white'}} className={stayle.dastur1}>
 Siz istalgan vaqtda har qanday mavzuda kurslarni o’rganishingiz 
 mumkin. Buning uchun qidiruv tizimidan foydalaning
 </div>
@@ -31,12 +32,12 @@ mumkin. Buning uchun qidiruv tizimidan foydalaning
 <Form>
 <InputGroup className="mb-3">
     <FormControl className={stayle.soz}
- style={{border:'none'}}
+ style={{border:'none',}}
  placeholder="Qanday kurs o`rganmoqchisiz?"
       aria-label="Recipient's username"
       aria-describedby="basic-addon2"
     /><div className={stayle.sea}>
-    <i class="fa fa-search"  aria-hidden="true"></i>
+    <i class="fa fa-search" style={{paddingBottom:'35px',marginRight:'16px'}} aria-hidden="true"></i>
 </div>
   </InputGroup>
 
@@ -44,72 +45,83 @@ mumkin. Buning uchun qidiruv tizimidan foydalaning
 </div>
 
 
+<Row style={{marginTop:'100px', width:'50%',}}>
 
-
-
+<Col>
 <div className={stayle.card1}>
 <div className={stayle.card11}>Marketting</div>
 <div className={stayle.som1}>250.000<span>UZS</span></div>
 <div className={stayle.Sharx1}>Raqamli marketing bo'yicha 
 to'liq kurs<span>|48 soat</span></div>
-<div className={stayle.star1}>
-<i class="fa fa-star"  aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
+<div style={{display:'flex'}} className={stayle.star1}>
+<ReactStars
+    count={5}
+    onChange={ratingChanged}
+    size={24}
+    activeColor="#ffd700"
+  />
 <div className={stayle.top1}>4.1 <span>(560)</span></div>
 
 </div>
 </div>
+</Col><Col>
+
 
 <div className={stayle.card2}>
 <div className={stayle.card12}>Dasturlash</div>
 <div className={stayle.som2}>350.000<span>UZS</span></div>
 <div className={stayle.Sharx2}>Java Spring bo’yicha to’liq
 kurs(amaliy)<span>|64 soat</span></div>
-<div className={stayle.star1}>
-<i class="fa fa-star"  aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
+<div style={{display:'flex'}} className={stayle.star1}>
+<ReactStars
+    count={5}
+    onChange={ratingChanged}
+    size={24}
+    activeColor="#ffd700"
+  />
 <div className={stayle.top1}>4.1 <span>(560)</span></div>
 
 </div>
 </div>
+</Col><Col>
+
 
 <div className={stayle.card3}>
 <div className={stayle.card13}>Dizayn</div>
 <div className={stayle.som3}>400.000<span>UZS</span></div>
 <div className={stayle.Sharx3}>Illustrator 0 dan professionalgacha
 to’liq kurs)<span>|72 soat</span></div>
-<div className={stayle.star1}>
-<i class="fa fa-star"  aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
+<div style={{display:'flex'}} className={stayle.star1}>
+<ReactStars
+    count={5}
+    onChange={ratingChanged}
+    size={24}
+    activeColor="#ffd700"
+  />
 
 <div className={stayle.top1}>4.1 <span>(560)</span></div>
 </div>
 </div>
+</Col><Col>
 
 <div className={stayle.card4}>
 <div className={stayle.card14}>Bizness</div>
 <div className={stayle.som4}>250.000<span>UZS</span></div>
 <div className={stayle.Sharx4}>Shaxsiy biznersni rivojlantirishda
 e’tibor berish kerak bo’lgan<span>|48 soat</span></div>
-<div className={stayle.star1}>
-<i class="fa fa-star"  aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<i class="fa fa-star" aria-hidden="true"></i>
-<div className={stayle.tor1}>4.1 <span>(560)</span></div>
+<div className={stayle.star1} style={{display:'flex'}}>
+<ReactStars
+    count={5}
+    onChange={ratingChanged}
+    size={24}
+    activeColor="#ffd700"
+  />
+<div className={stayle.tor1} >4.1 <span style={{paddingTop:'-7px',backgroundColor:'white'}}>(560)</span></div>
+</div>
 </div>
 
-</div>
+</Col>
+</Row>
  </div>
 
 
