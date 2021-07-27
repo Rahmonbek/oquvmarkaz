4807ea09a4415a5f6336688836f350f378f4351c
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-07-27 12:47:21 +0500

    Create FreeApp.js

---
+++ src/pages/Course/FreeApp.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Header from './pages/Header'
+import Footer from './pages/Footer'
+import About from './pages/About'
+import Main from './pages/Main'
+export default class FreeApp extends Component {
+    render() {
+        return (
+            <div>
+             <Header />
+             <Footer />
+             <About />
+             <Main /> 
+            </div>
+        )
+    }
+}
