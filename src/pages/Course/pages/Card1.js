dd3d853dc714c3a8b311f8583402c1a1141d7d9c
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-07-28 18:07:27 +0500

    28-iyun
    lsdkf

--- src/pages/Course/pages/Card1.js
+++ src/pages/Course/pages/Card1.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css';
 import style from '../css/Card.module.css'
-import { Drawer, Button } from 'antd';
+
 import '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import Func from './Func'
+import { Button, Col, Row } from 'react-bootstrap';
+import img1 from '../images/ellips1.png'
+import img2 from '../images/ellips2.png'
+import img3 from '../images/ellips3.png'
+import img4 from '../images/ellips4.png'
 
 
 export default class Card1 extends Component {
@@ -31,7 +30,7 @@ export default class Card1 extends Component {
 
 
         return (
-            <div>
+            <div >
     <div className={style.cardall}>
 <div style={{marginTop:'24px'}} className={style.dasturlash} >Dasturlash</div>
 <div style={{ display:'flex', paddingLeft:'20px',paddingTop:'104px'}}>  
@@ -47,133 +46,41 @@ export default class Card1 extends Component {
 </div>
 
 
-<div style={{paddingTop:'71px',fontSize: '18px', paddingLeft:'24px'}}>Kurs bo`limlari:
-     </div>
+<div style={{paddingTop:'71px',fontSize: '18px', paddingLeft:'24px'}}>Kurs bo`limlari:</div>
+     
 
+<Func/>
 
-<Accordion style={{backgroundColor:' #F1F2F6'}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-label="Expand"
-          aria-controls="additional-actions1-content"
-          id="additional-actions1-header"
-        >
-          <FormControlLabel
-            aria-label="Acknowledge"
-            onClick={(event) => event.stopPropagation()}
-            onFocus={(event) => event.stopPropagation()}
-            control={<Checkbox onClick={this.showDrawer} />}
-            label="Dasturlashga kirish va o’rnatish jarayoni"
-          />
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography color="textSecondary">
-          <div style={{ display:'flex', paddingLeft:'20px',paddingTop:'34px'}}>  
-    
-    <div style={{ display:'block'}}>
-    <h6 style={{opacity:'0.7'}}>kurs narxlari</h6>
-    <h3 style={{fontWeight:'600'}}>250.000<span sytle={{fontSize: '14px',textAlign: 'left',fontWeight:'200'}}> uzs</span></h3>
-    </div>
-    <div style={{ display:'block', paddingLeft:'43px'}}>
-    <h6 style={{opacity:'0.7'}}>Kurs hajmi</h6>
-    <h3 style={{fontWeight:'600'}}>38 soat</h3>
-    </div>
-    </div>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion  style={{backgroundColor:' #F1F2F6'}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-label="Expand"
-          aria-controls="additional-actions1-content"
-          id="additional-actions1-header"
-        >
-          <FormControlLabel
-            aria-label="Acknowledge"
-            onClick={(event) => event.stopPropagation()}
-            onFocus={(event) => event.stopPropagation()}
-            control={<Checkbox onClick={this.showDrawer} />}
-            label="Dasturda oddiy amallar bilan ishlash"
-          />
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography color="textSecondary">
-          <div style={{ display:'flex', paddingLeft:'20px',paddingTop:'34px'}}>  
-    
-    <div style={{ display:'block'}}>
-    <h6 style={{opacity:'0.7'}}>kurs narxlari</h6>
-    <h3 style={{fontWeight:'600'}}>450.000<span sytle={{fontSize: '14px',textAlign: 'left',fontWeight:'200'}}> uzs</span></h3>
-    </div>
-    <div style={{ display:'block', paddingLeft:'43px'}}>
-    <h6 style={{opacity:'0.7'}}>Kurs hajmi</h6>
-    <h3 style={{fontWeight:'600'}}>38 soat</h3>
-    </div>
-    </div>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion  style={{backgroundColor:' #F1F2F6'}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-label="Expand"
-          aria-controls="additional-actions1-content"
-          id="additional-actions1-header"
-        >
-          <FormControlLabel
-            aria-label="Acknowledge"
-            onClick={(event) => event.stopPropagation()}
-            onFocus={(event) => event.stopPropagation()}
-            control={<Checkbox onClick={this.showDrawer}  />}
-            label=" OOP dasturlash asoslari"
-          />
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography color="textSecondary">
-          <div style={{ display:'flex', paddingLeft:'20px',paddingTop:'34px'}}>  
-    
-    <div style={{ display:'block'}}>
-    <h6 style={{opacity:'0.7'}}>kurs narxlari</h6>
-    <h3 style={{fontWeight:'600'}}>150.000<span sytle={{fontSize: '14px',textAlign: 'left',fontWeight:'200'}}> uzs</span></h3>
-    </div>
-    <div style={{ display:'block', paddingLeft:'43px'}}>
-    <h6 style={{opacity:'0.7'}}>Kurs hajmi</h6>
-    <h3 style={{fontWeight:'600'}}>38 soat</h3>
-    </div>
-    </div>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+ </div>  
+ <div style={{marginTop:'42px'}} className={style.cardall}>
+<div className={style.title12}>
+<p style={{paddingTop:'24px',marginLeft:'24px' , color:'#2E2E2E',fontSize:'18px'}}>Boshqa Mentorlar</p>
+<Button variant="light" style={{borderRadius:'50%', border: '1px solid #9DA7BB',marginTop:'20px',marginLeft:"30px",mardin:'auto',width:'40px',height:'40px'}}><i class="fa fa-arrow-left" aria-hidden="true"></i></Button>
+<Button variant="light" style={{borderRadius:'50%', border: '1px solid #9DA7BB',marginTop:'20px',marginLeft:"30px",mardin:'auto',width:'40px',height:'40px'}}><i class="fa fa-arrow-right" aria-hidden="true"></i></Button>
+
+</div>
+<hr/>
+
+<div style={{paddingLeft:'24px'}} className={style.mentor}>
+<Row lg={2}>
+<Col><div className={style.back1}><img src={img1}/>
+<div className={style.title24}> Abbas</div><div className={style.title23}>Arxitektor</div></div> </Col>
+<Col><div  className={style.back2}><img src={img2}/>
+<div className={style.title24}>Tahir</div><div className={style.title23}>Traktorchi</div></div></Col>
+<Col><div className={style.back3}><img src={img3}/>
+<div className={style.title24}>Mexmut</div><div className={style.title23}>Dizayner</div></div></Col>
+<Col><div className={style.back4}><img src={img4}/>
+<div className={style.title24}>Tom Jonson</div><div className={style.title23}>Dasturlovchi</div></div></Col>
+</Row>
+</div>
+
+<div className={style.title45}>Barchasini ko’rish</div>
+ </div>
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-    </div> <Drawer
-          title="Basic Drawer"
-          placement="bottom"
-          closable={false}
-          onClose={this.onClose}
-          visible={this.state.visible}
-          getContainer={false}
-          style={{ position: 'absolute', width:'20%'}}
-        >
-          <p style={{display:'flex'}}>
-          <Button type="primary" style={{ height:'50px'}} >
-            <p style={{fontSize:'22px',display:'flex',justifyContent:'center'}} >  sotib olish  |  150.000  uzs  </p>
-          </Button>
-         
-          </p>
-        </Drawer>  
             </div>
         )
     }
