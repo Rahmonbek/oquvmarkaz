8f5735e8e49755ab4b8928e1fb22bf2b03f9012c
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-08-05 10:23:03 +0500

    ws;dpfo
    as'dp

--- src/pages/dashboard/pages/Bosh5.js
+++ src/pages/dashboard/pages/Bosh5.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react'
-import images2 from '../img/tasvir1.png';
-import images3 from '../img/tasvir2.png';
+import React, { Component } from "react";
+import images2 from "../img/tasvir1.png";
+import images3 from "../img/tasvir2.png";
 
-import stayle4 from '../css/Navbar4.module.css'
-import { Container } from 'react-bootstrap';
+import stayle4 from "../css/Navbar4.module.css";
+import { Container } from "react-bootstrap";
 
 export default class Bosh5 extends Component {
-    
-    
-    render() {
-        return (
-            <div>
-             <div style={{position:'relative'}} className={stayle4.tasvir}>
-  <img  style={{fontSize:'3vw',width:'7%',height:'7%' , paddingLeft:'6%'}} src={images2}/>             
- <Container><div style={{color:'white', fontSize:'3vw',paddingLeft:'10%'}}>“O’z qalbing va ichki sezgingga ergashish uchun 
-yetarlicha jur’atli bo’l. Zotan shu ikki tuyg’u 
-sening aslida kim bo’lishni xoxlashingni aniq biladi.”</div></Container>
-
-<img style={{fontSize:'3vw',width:'5%' , height:'6%', position:'absolute',right:'80px',bottom:'80px' }} src={images3}/>
-<div  style={{color:'white', fontSize:'3vw',marginTop:'30px',paddingBottom:'50px',paddingLeft:'20%'}} >Stiv Jobs</div>
-
-                 </div>   
+  render() {
+    return (
+      <div>
+        <div className={stayle4.tasvir}>
+          <Container>
+            <div className={stayle4.TextMeCenter}>
+              <p className={stayle4.standUpOpen}>,,</p>
+              <p className={stayle4.standUpInText}>
+                “O’z qalbing va ichki sezgingga ergashish uchun yetarlicha
+                jur’atli bo’l. Zotan shu ikki tuyg’u sening aslida kim bo’lishni
+                xoxlashingni aniq biladi.”
+                <p className={stayle4.StivName}>Stiv Jobs</p>
+              </p>
+              <p className={stayle4.standUpClose}>,,</p>
             </div>
-        )
-    }
+          </Container> 
+
+         
+        </div>
+      </div>
+    );
+  }
 }
